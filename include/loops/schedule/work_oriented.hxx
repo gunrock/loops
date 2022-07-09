ab428802a3061532e7b9f1fbbacdedfe8e786c08
neoblizz <osama94@gmail.com>
2022-07-09 16:35:09 -0700

    Comments + More debugging for larger datasets.

--- include/loops/schedule/work_oriented.hxx
+++ include/loops/schedule/work_oriented.hxx
@@ -23,11 +23,29 @@
 namespace loops {
 namespace schedule {
 
-template <class T, class U>
-__host__ __device__ constexpr auto div(T const& t, U const& u) {
+/**
+ * @brief Simple safe division: (a + b - 1) / b.
+ *
+ * @tparam type_t_t Type of the dividend.
+ * @tparam type_u_t Type of the divisor.
+ * @param t Dividend.
+ * @param u Divisor.
+ * @return The quotient.
+ */
+template <class type_t_t, class type_u_t>
+__host__ __device__ constexpr auto div(type_t_t const& t, type_u_t const& u) {
   return (t + u - 1) / u;
 }
 
+/**
+ * @brief Traits for Atom.
+ *
+ * @todo Implement an atom iterator, right now it is based on CSR only. Can be
+ * abstracted very simply by allowing UDF iterators.
+ *
+ * @tparam atoms_type Type of the atoms.
+ * @tparam atom_size_type Type of the atom size.
+ */
 template <typename atoms_type, typename atom_size_type>
 class atom_traits<algorithms_t::work_oriented, atoms_type, atom_size_type> {
  public:
@@ -50,6 +68,15 @@ class atom_traits<algorithms_t::work_oriented, atoms_type, atom_size_type> {
   atoms_iterator_t atoms_;
 };
 
+/**
+ * @brief Traits for Tile.
+ *
+ * @todo Implement an tile iterator, right now it is based on CSR only. Can be
+ * abstracted very simply by allowing UDF iterators.
+ *
+ * @tparam tiles_type Type of the tiles.
+ * @tparam tile_size_type Type of the tile size (default: std::size_t).
+ */
 template <typename tiles_type, typename tile_size_type>
 class tile_traits<algorithms_t::work_oriented, tiles_type, tile_size_type> {
  public:
@@ -70,6 +97,16 @@ class tile_traits<algorithms_t::work_oriented, tiles_type, tile_size_type> {
   tiles_iterator_t tiles_;
 };
 
+/**
+ * @brief Work-oriented schedule's setup interface.
+ *
+ * @tparam THREADS_PER_BLOCK Threads per block.
+ * @tparam ITEMS_PER_THREAD Number of Items per thread to process.
+ * @tparam tiles_type Type of the tiles.
+ * @tparam atoms_type Type of the atoms.
+ * @tparam tile_size_type Type of the tile size.
+ * @tparam atom_size_type Type of the atom size.
+ */
 template <std::size_t THREADS_PER_BLOCK,
           std::size_t ITEMS_PER_THREAD,
           typename tiles_type,
@@ -89,12 +126,12 @@ class setup<algorithms_t::work_oriented,
                                                  atoms_type,
                                                  atom_size_type> {
  public:
-  using tiles_t = tiles_type;
-  using atoms_t = atoms_type;
-  using tiles_iterator_t = tiles_t*;
-  using atoms_iterator_t = atoms_t*;
-  using tile_size_t = tile_size_type;
-  using atom_size_t = atom_size_type;
+  using tiles_t = tiles_type;          /// Tile Type
+  using atoms_t = atoms_type;          /// Atom Type
+  using tiles_iterator_t = tiles_t*;   /// Tile Iterator Type
+  using atoms_iterator_t = tiles_t*;   /// Atom Iterator Type
+  using tile_size_t = tile_size_type;  /// Tile Size Type
+  using atom_size_t = atom_size_type;  /// Atom Size Type
 
   using tile_traits_t =
       tile_traits<algorithms_t::work_oriented, tiles_type, tile_size_type>;
@@ -120,23 +157,19 @@ class setup<algorithms_t::work_oriented,
       : tile_traits_t(_num_tiles, _tiles),
         atom_traits_t(_num_atoms),
         total_work(_num_tiles + _num_atoms),
-        num_threads(threads_per_block),  // items_per_tile * (div(total_work,
-                                         // items_per_tile))),
+        num_threads(gridDim.x * threads_per_block),
         work_per_thread(div(total_work, num_threads)) {}
 
   __device__ __forceinline__ auto init() {
     thrust::counting_iterator<atoms_t> atoms_indices;
 
     std::size_t tid = threadIdx.x + blockIdx.x * blockDim.x;
-    // int hid = (blockIdx.x * gridDim.y) + blockIdx.y;
-    // if (threadIdx.x < 2) {
     std::size_t upper = min(work_per_thread * tid, total_work);
     std::size_t lower = min(upper + work_per_thread, total_work);
     auto st = search(upper, tile_traits_t::begin() + 1, atoms_indices,
                      tile_traits_t::size(), atom_traits_t::size());
     auto en = search(lower, tile_traits_t::begin() + 1, atoms_indices,
                      tile_traits_t::size(), atom_traits_t::size());
-    // }
     return thrust::make_pair(st, en);
   }
 
@@ -172,11 +205,11 @@ class setup<algorithms_t::work_oriented,
   /**
    * @brief Thrust based 2D binary-search for merge-path algorithm.
    *
-   * @param diagonal
-   * @param a
-   * @param b
-   * @param a_len
-   * @param b_len
+   * @param diagonal Diagonal of the search.
+   * @param a First iterator.
+   * @param b Second iterator.
+   * @param a_len Length of the first iterator.
+   * @param b_len Length of the second iterator.
    * @return A coordinate.
    */
   template <typename xit_t, typename yit_t>
