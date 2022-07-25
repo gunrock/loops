2b3df354b477a8211c4872f0d793136ec5be9d01
neoblizz <osama94@gmail.com>
2022-07-25 10:37:59 -0700

    Clean-up: header files and junk

--- benchmarks/spmv/parameters.hxx
+++ benchmarks/spmv/parameters.hxx
@@ -17,6 +17,8 @@
 
 #include <loops/util/filepath.hxx>
 
+std::string filename;
+
 struct parameters_t {
   std::string filename;
   bool help = false;
--- benchmarks/spmv/work_oriented.cu
+++ benchmarks/spmv/work_oriented.cu
@@ -11,11 +11,8 @@
 
 #include "parameters.hxx"
 
-#include <loops/memory.hxx>
-#include <loops/container/vector.hxx>
-#include <loops/util/device.hxx>
-#include <loops/util/sample.hxx>
 #include <loops/util/generate.hxx>
+#include <loops/container/container.hxx>
 #include <loops/algorithms/spmv/work_oriented.cuh>
 
 #include <nvbench/nvbench.cuh>
@@ -30,17 +27,14 @@ void work_oriented_bench(nvbench::state& state, nvbench::type_list<value_t>) {
   using offset_t = int;
   using type_t = value_t;
 
-  /// Get sample CSR matrix and create x, y vectors.
-  std::size_t rows = 1 << 20;
-  std::size_t cols = 1 << 20;
-
-  csr_t<index_t, offset_t, value_t> csr;
-  generate::random::csr<index_t, offset_t, type_t>(rows, cols, 0.0000001, csr);
+  matrix_market_t<index_t, offset_t, type_t> mtx;
+  csr_t<index_t, offset_t, type_t> csr(mtx.load(filename));
 
   vector_t<type_t> x(csr.rows);
   vector_t<type_t> y(csr.rows);
 
-  generate::random::uniform_distribution(x.begin(), x.end(), 1, 10);
+  generate::random::uniform_distribution(x.begin(), x.end(), type_t(1.0),
+                                         type_t(10.0));
 
 #if LOOPS_CUPTI_SUPPORTED
   /// Add CUPTI metrics to collect for the state.
@@ -60,3 +54,27 @@ void work_oriented_bench(nvbench::state& state, nvbench::type_list<value_t>) {
 // Define a type_list to use for the type axis:
 using value_types = nvbench::type_list<int, float, double>;
 NVBENCH_BENCH_TYPES(work_oriented_bench, NVBENCH_TYPE_AXES(value_types));
+
+int main(int argc, char** argv) {
+  parameters_t params(argc, argv);
+  filename = params.filename;
+
+  if (params.help) {
+    // Print NVBench help.
+    const char* args[1] = {"-h"};
+    NVBENCH_MAIN_BODY(1, args);
+  } else {
+    // Create a new argument array without matrix filename to pass to NVBench.
+    char* args[argc - 2];
+    int j = 0;
+    for (int i = 0; i < argc; i++) {
+      if (strcmp(argv[i], "--market") == 0 || strcmp(argv[i], "-m") == 0) {
+        i++;
+        continue;
+      }
+      args[j] = argv[i];
+      j++;
+    }
+    NVBENCH_MAIN_BODY(argc - 2, args);
+  }
+}
