32317777715cb3284cf028aa7978641ba7740c93
neoblizz <osama94@gmail.com>
2022-11-22 11:43:23 -0800

    Experimentation (reproduce results).

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 # Warning, a complete run can take days.
-DATASET_DIR="/data/suitesparse_dataset"
+DATASET_DIR="$HOME/suitesparse"
 DATASET_FILES_NAMES="../datasets/suitesparse.txt"
 BINARY="../build/bin"
-ALG=${1:-"4"}
 
 SPMV[0]="work_oriented"
 SPMV[1]="group_mapped"
@@ -15,9 +14,10 @@ EXE_PREFIX="loops.spmv"
 
 for i in {0..4} 
 do
+  echo "kernel,dataset,rows,cols,nnzs,elapsed" >> spmv_log.${SPMV[$i]}.csv 
   while read -r DATA
   do
-    echo $BINARY/$EXE_PREFIX.${SPMV[$ALG]} -m $DATASET_DIR/$DATA
-    timeout 200 $BINARY/$EXE_PREFIX.${SPMV[$ALG]} -m $DATASET_DIR/$DATA >> spmv_log.${SPMV[$ALG]}.txt 
-  done < "$DATASET_DIR/$DATASET_FILES"
+    echo $BINARY/$EXE_PREFIX.${SPMV[$i]} -m $DATASET_DIR/$DATA
+    timeout 60 $BINARY/$EXE_PREFIX.${SPMV[$i]} -m $DATASET_DIR/$DATA >> spmv_log.${SPMV[$i]}.csv 
+  done < "$DATASET_FILES_NAMES"
 done
