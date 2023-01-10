a1f248c5ef0df19c48ca1cda7efd477d207c01ab
Muhammad Osama <osama94@gmail.com>
2023-01-10 13:16:26 -0800

    Update rocsparse.cu

--- examples/spmv/rocsparse.cu
+++ examples/spmv/rocsparse.cu
@@ -11,15 +11,23 @@
 
 #include "helpers.hxx"
 #include <rocsparse.h>
+#include <tuple>
 
 using namespace loops;
 
 template <typename index_t, typename offset_t, typename type_t>
-double roc_csrmv(csr_t<index_t, offset_t, type_t>& csr,
+auto roc_csrmv(csr_t<index_t, offset_t, type_t>& csr,
                  vector_t<type_t>& x,
-                 vector_t<type_t>& y) {
+                 vector_t<type_t>& y,
+                 int num_runs = 100) {
+  
+  hipStream_t stream;
+  hipStreamCreate(&stream);
+
   rocsparse_handle handle;
   rocsparse_create_handle(&handle);
+
+  rocsparse_set_stream(handle, stream);
 
   rocsparse_mat_descr descr;
   rocsparse_create_mat_descr(&descr);
@@ -28,30 +36,50 @@ double roc_csrmv(csr_t<index_t, offset_t, type_t>& csr,
   rocsparse_mat_info info;
   rocsparse_create_mat_info(&info);
 
+  util::timer_t preprocess;
+  preprocess.start();
   // Perform analysis step to obtain meta data
   rocsparse_scsrmv_analysis(handle, rocsparse_operation_none, csr.rows,
                             csr.cols, csr.nnzs, descr, csr.values.data().get(),
                             csr.offsets.data().get(), csr.indices.data().get(),
                             info);
 
-  util::timer_t timer;
-  timer.start();
+  preprocess.stop();
 
-  // Compute y = Ax
+  
+
   type_t beta = 0;
   type_t alpha = 1;
+
+  // Perform a warm-up run:
   rocsparse_scsrmv(handle, rocsparse_operation_none, csr.rows, csr.cols,
                    csr.nnzs, &alpha, descr, csr.values.data().get(),
                    csr.offsets.data().get(), csr.indices.data().get(), info,
                    x.data().get(), &beta, y.data().get());
-  timer.stop();
 
+  double time = 0;
+  util::timer_t timer;
+  
+  // Compute y = Ax
+  for (auto i = 0; i < num_runs; ++i) {
+    timer.start();
+    rocsparse_scsrmv(handle, rocsparse_operation_none, csr.rows, csr.cols,
+                   csr.nnzs, &alpha, descr, csr.values.data().get(),
+                   csr.offsets.data().get(), csr.indices.data().get(), info,
+                   x.data().get(), &beta, y.data().get());
+    timer.stop();
+    time += timer.milliseconds();
+  }
+  
+  time /= num_runs;
+  
   // Clean up
   rocsparse_destroy_mat_info(info);
   rocsparse_destroy_mat_descr(descr);
   rocsparse_destroy_handle(handle);
+  hipStreamDestroy(stream);
 
-  return timer.milliseconds();
+  return std::make_tuple(time, preprocess.milliseconds());
 }
 
 int main(int argc, char** argv) {
@@ -73,12 +101,12 @@ int main(int argc, char** argv) {
   generate::random::uniform_distribution(x.begin(), x.end(), 1, 10);
 
   // Run the benchmark.
-  auto elapsed = roc_csrmv(csr, x, y);
+  auto [elapsed, preprocess] = roc_csrmv(csr, x, y);
 
   std::cout << "rocsparse," << mtx.dataset << "," << csr.rows << "," << csr.cols
-            << "," << csr.nnzs << "," << elapsed << std::endl;
+            << "," << csr.nnzs << "," << std::fixed << std::setprecision(6) << elapsed << "," << preprocess << std::endl;
 
-  //   // Validation.
-  //   if (parameters.validate)
-  //     cpu::validate(parameters, csr, x, y);
+  // Validation.
+  if (parameters.validate)
+    cpu::validate(parameters, csr, x, y);
 }
